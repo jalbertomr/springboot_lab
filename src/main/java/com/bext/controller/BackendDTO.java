f7a044577ca97d631a5728ffa2936f8977bc7b26
bext <jalbertomr@yahoo.com>
2019-07-10 15:49:14 -0500

    add BackendDTO

---
+++ src/main/java/com/bext/controller/BackendDTO.java
@@ -0,0 +1,28 @@
+package com.bext.controller;
+
+public class BackendDTO {
+	private String mensaje;
+	private long time;
+	private String ip;
+	
+	public String getMensaje() {
+		return mensaje;
+	}
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	public long getTime() {
+		return time;
+	}
+	public void setTime(long time) {
+		this.time = time;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	
+	
+}
